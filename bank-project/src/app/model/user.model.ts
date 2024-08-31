b55aa6311aa50fb1950e9e9072248c6acde112d6
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-08-31 23:07:32 +0600

    nothing

---
+++ bank-project/src/app/model/user.model.ts
@@ -0,0 +1,11 @@
+export class UserModel {
+    
+    id !: string;
+    firstName!: string;
+    lastName!: string;
+    gender!: string;
+    address!: string;
+    email !: string;
+    password !: string;
+    role !: string;
+}
